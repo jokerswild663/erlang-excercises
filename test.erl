fc72ce41eb93677aea8dff3c3a7213d97ce8fb9a
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-06 16:11:54 -0700

    added exponent function to test.erl

--- test.erl
+++ test.erl
@@ -1,8 +1,13 @@
 -module(test).
--export([fac/1,double/1]).
+-export([fac/1,double/1,exp/2]).
 
 fac(0) -> 1;
 fac(N) -> N * fac(N-1).
 
 double(0) -> 0;
 double(N) -> N * 2.
+
+exp(0,P) -> 0;
+exp(X,0) -> 1;
+exp(X,1) -> X;
+exp(X,P) -> X * exp(X,P-1).
