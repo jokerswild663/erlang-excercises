bdc982f4e8aa6d461d15d0e46dfacf956f0c7e5d
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-06 17:13:54 -0700

    added inch / centimeter converter function

--- test.erl
+++ test.erl
@@ -1,5 +1,5 @@
 -module(test).
--export([fac/1,double/1,exp/2]).
+-export([fac/1,double/1,exp/2,converter/1]).
 
 fac(0) -> 1;
 fac(N) -> N * fac(N-1).
@@ -11,3 +11,6 @@ exp(0,P) -> 0;
 exp(X,0) -> 1;
 exp(X,1) -> X;
 exp(X,P) -> X * exp(X,P-1).
+
+converter({inch, X}) -> {centimeter, X*2.54};
+converter({centimeter, Y}) -> {inch, Y/2.54}.
