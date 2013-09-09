33a63921598eee231ff62af3e2076f00eec9f9cb
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-09 15:54:44 -0700

    added min_list to list_test.erl

--- list_test.erl
+++ list_test.erl
@@ -1,5 +1,5 @@
 -module(list_test).
--export([list_length/1,max_list/1]).
+-export([list_length/1,max_list/1,min_list/1]).
 
 list_length([]) -> 0;
 list_length([First | Rest]) -> 1 + list_length(Rest).
@@ -11,5 +11,15 @@ max_list([Head | Rest]) -> max_list(Rest,Head).
 max_list([], Result_so_far) -> Result_so_far;
 max_list([Head | Remaining], Result_so_far) when Head > Result_so_far -> max_list(Remaining, Head);
 max_list([Head | Remaining], Result_so_far) -> max_list(Remaining, Result_so_far).
+
+%*************************************************************
+
+%*****************************Min list************************
+
+min_list([Head | Rest]) -> min_list(Rest,Head).
+
+min_list([], Result_so_far) -> Result_so_far;
+min_list([Head | Remnant], Result_so_far) when Head < Result_so_far -> min_list(Remnant, Head);
+min_list([Head | Remnant], Result_so_far) -> min_list(Remnant, Result_so_far).
 
 %*************************************************************
