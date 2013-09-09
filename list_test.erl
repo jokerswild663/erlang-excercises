1beb80c43ab5da6b49df5e60a4973c9e7b29166c
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-09 16:35:21 -0700

    added reverse_list to list_test.erl

--- list_test.erl
+++ list_test.erl
@@ -1,5 +1,5 @@
 -module(list_test).
--export([list_length/1,max_list/1,min_list/1]).
+-export([list_length/1,max_list/1,min_list/1,reverse_list/1]).
 
 list_length([]) -> 0;
 list_length([First | Rest]) -> 1 + list_length(Rest).
@@ -21,5 +21,14 @@ min_list([Head | Rest]) -> min_list(Rest,Head).
 min_list([], Result_so_far) -> Result_so_far;
 min_list([Head | Remnant], Result_so_far) when Head < Result_so_far -> min_list(Remnant, Head);
 min_list([Head | Remnant], Result_so_far) -> min_list(Remnant, Result_so_far).
+
+%*************************************************************
+
+%*************************Reverse list************************
+
+reverse_list(Original_list) -> reverse_list(Original_list, []).
+
+reverse_list([Head | First_List], New_List) -> reverse_list(First_List, [Head | New_List]);
+reverse_list([], New_List) -> New_List.
 
 %*************************************************************
