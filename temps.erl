fb58096b1113e964ed77c40eea2e91e1928e252c
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-09 21:22:47 -0700

    added ability to find max temperature: find_max

--- temps.erl
+++ temps.erl
@@ -1,5 +1,5 @@
 -module(temps).
--export([format_temps/1, find_min/1]).
+-export([format_temps/1, find_min/1,find_max/1]).
 
 %*************Temperature conversions*********************************
 format_temps([]) -> ok;
@@ -8,6 +8,8 @@ format_temps(RestCities).
 
 convert_to_celsius({Name, {c,Temp}}) -> {Name, {c,Temp}};
 convert_to_celsius({Name, {f,Temp}}) -> {Name, {c,(Temp - 32) * 5 / 9}}.
+
+
 
 find_min([Head | City_List]) -> find_min(City_List, Head).
 
@@ -19,6 +21,18 @@ if
 
   Min_temp_so_far >= City_temp -> 
     find_min(City_List, {City_Name, {c, City_temp}})
+end.
+
+find_max([Head | City_List]) -> find_max(City_List, Head).
+
+find_max([],City) -> City;
+find_max([{City_Name, {c,Current_temp}} | City_List], {Name, {c,Max_temp_so_far}}) ->
+if
+  Current_temp > Max_temp_so_far ->
+    find_max(City_List, {City_Name, {c,Current_temp}});
+
+  Current_temp =< Max_temp_so_far -> 
+    find_max(City_List, {Name, {c, Max_temp_so_far}})
 end.
 
 print_temp({Name, {C,Temp}}) -> io:format("~-15w ~w c~n", [Name,Temp]).
