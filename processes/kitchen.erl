f17843b5e44f4dc01e8016c1edb3fe65701ce6d1
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-21 17:38:21 -0700

    added fridge2 to kitchen.  This stores data in a list to pass between processes

--- processes/kitchen.erl
+++ processes/kitchen.erl
@@ -14,3 +14,20 @@ fridge() ->
     terminate ->
       ok
   end.
+
+fridge2(FoodsList) ->
+  receive
+    {From, {store, Food}} ->
+      From ! {self(), {Food,ok}},
+      fridge2([Food | FoodsList]);
+    {From, {take, Food}} ->
+      case lists:member(Food, FoodsList) of
+        true ->
+          From ! {self(), {Food,ok}},
+          fridge2(lists:delete(Food, FoodsList));
+        false->
+          From ! {self(), {Food,not_found}}
+      end;
+    terminate -> 
+      ok
+  end.
