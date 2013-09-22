2759192fb1397fa95a24a2f664e501c2fbbf8bdc
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-22 13:06:20 -0700

    added store/2 and take/2 in order to abstract away from accessing kitchen2 directly

--- processes/kitchen.erl
+++ processes/kitchen.erl
@@ -31,3 +31,15 @@ fridge2(FoodsList) ->
     terminate -> 
       ok
   end.
+
+store(Pid,Food) ->
+  Pid ! {self(), {store,Food}},
+  receive
+    {Pid, Msg} -> Msg
+  end.
+
+take(Pid, Food) ->
+  Pid ! {self(), {take,Food}}, 
+  receive
+    {Pid, Msg} -> Msg
+  end.
