16212cbadacc911122f3e73bd5edb4d4c9ba4aad
jeffw <joker@joker-Latitude-D630.(none)>
2013-09-25 19:39:02 -0700

    added chain/0 which creates multiple processes and links them together

--- processes/linkmon.erl
+++ processes/linkmon.erl
@@ -4,3 +4,16 @@
 myproc() ->
   timer:sleep(5000),
   exit(something).
+
+chain(0) ->
+  receive
+    _ -> ok
+  after 2000 ->
+    exit("death of chain starts here")
+  end;
+chain(N) ->
+  Pid = spawn(fun() -> chain(N-1) end),
+  link(Pid),
+  receive
+    _ -> ok
+  end.
